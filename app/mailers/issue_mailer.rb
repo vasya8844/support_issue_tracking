91046ff97100ec42559a13b49ce23ada04b9448e
Vasyl Boychuk <vasyaboychuk@gmail.com>
2017-07-06 13:43:49 +0300

    disable mail on production (heroku)

--- app/mailers/issue_mailer.rb
+++ app/mailers/issue_mailer.rb
@@ -1,12 +1,19 @@
 class IssueMailer < ApplicationMailer
   def issue_email(issue)
     @issue = issue
-    mail(to: @issue.reporter_email, subject: 'Issue reported')
+    send_mail(to: @issue.reporter_email, subject: 'Issue reported')
   end
 
   def comment_email(comment)
     @comment = comment
     @issue = @comment.issue
-    mail(to: @issue.reporter_email, subject: 'Stafff commented on your issue')
+    send_mail(to: @issue.reporter_email, subject: 'Stafff commented on your issue')
+  end
+
+  private
+  def send_mail(headers)
+    return if Rails.env == 'production'
+
+    mail(headers)
   end
 end
